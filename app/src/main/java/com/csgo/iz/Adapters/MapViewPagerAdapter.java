d24b9af4c759a8f02864c7d79fb495d2a3030c87
Chris Sawczuk <gummybassist@gmail.com>
2015-12-15 20:25:44 +0000

    Simplified view page adapters

--- app/src/main/java/com/csgo/iz/Adapters/MapViewPagerAdapter.java
+++ app/src/main/java/com/csgo/iz/Adapters/MapViewPagerAdapter.java
@@ -1,54 +1,33 @@
 package com.csgo.iz.Adapters;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-
-import com.csgo.iz.Fragments.AchievementItemFragment;
-import com.csgo.iz.Fragments.MapItemFragment;
-import com.csgo.iz.modal.bean.Achievement;
-import com.csgo.iz.modal.bean.Map;
-
-import android.content.Context;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 
+import com.csgo.iz.Fragments.MapItemFragment;
+import com.csgo.iz.modal.bean.Map;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class MapViewPagerAdapter extends FragmentPagerAdapter {
-	private int number_tabs = 4;
-	private Fragment fragment;
 	private String[] fragment_titles = {"Bomb Defusal", "Hostage", "Arms Race", "Demolition"};
-	private Context context;
 	private HashMap<String, List<Map>> map;
-	public MapViewPagerAdapter(FragmentManager fm,Context context,HashMap<String, List<Map>> map) {
+
+	public MapViewPagerAdapter(FragmentManager fm, HashMap<String, List<Map>> map) {
 		super(fm);
-		this.context = context;
 		this.map = map;
 	}
 	@Override
 	public CharSequence getPageTitle(int position) {
-		// TODO Auto-generated method stub
 		return fragment_titles[position];
 	}
 
 	@Override
 	public Fragment getItem(int position) {
 		String[] KEYS = {"DEFUSAL","ARMS_RACE","HOSTAGE","DEMOLITION"};
-		switch(position){
-		case 0:
-			fragment = MapItemFragment.InstanceOf(getAchievementList(KEYS[0]));
-			break;
-		case 1:
-			fragment = MapItemFragment.InstanceOf(getAchievementList(KEYS[2]));
-			break;
-		case 2:
-			fragment = MapItemFragment.InstanceOf(getAchievementList(KEYS[1]));
-			break;
-		case 3:
-			fragment = MapItemFragment.InstanceOf(getAchievementList(KEYS[3]));
-			break;
-		}
-		return fragment;
+		return MapItemFragment.InstanceOf(getAchievementList(KEYS[position]));
 	}
 
 	public ArrayList<Map> getAchievementList(String keyPosition) {
@@ -56,14 +35,12 @@ public class MapViewPagerAdapter extends FragmentPagerAdapter {
 	}
 	@Override
 	public int getItemPosition(Object object) {
-		// TODO Auto-generated method stub
 		return POSITION_NONE;
 	}
 
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
-		return number_tabs;
+		return 4;
 	}
 
 }
--- app/src/main/java/com/csgo/iz/Adapters/WeaponViewPagerAdapter.java
+++ app/src/main/java/com/csgo/iz/Adapters/WeaponViewPagerAdapter.java
@@ -1,69 +1,50 @@
 package com.csgo.iz.Adapters;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 
 import com.csgo.iz.Fragments.WeaponItemFragment;
 import com.csgo.iz.modal.bean.Weapon;
 
-import android.content.Context;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.util.Log;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class WeaponViewPagerAdapter extends FragmentPagerAdapter {
-	private int number_tabs = 4;
-	private Fragment fragment;
-	private String[] fragment_titles = {"Pistols", "Rifles", "SMG", "Heavy"};
-	private Context context;
-	private HashMap<String, List<Weapon>> map;
-	public WeaponViewPagerAdapter(FragmentManager fm,Context context,HashMap<String, List<Weapon>> map) {
-		super(fm);
-		this.context = context;
-		this.map = map;
-	}
-	@Override
-	public CharSequence getPageTitle(int position) {
-		// TODO Auto-generated method stub
-		return fragment_titles[position];
-	}
 
-	@Override
-	public Fragment getItem(int position) {
-		String[] KEYS = {"PISTOLS","RIFLES","SMG","HEAVY"};
-		switch(position){
-		case 0:
-			Log.v("Weapon Tab Stat", "WeaponViewPager getItem(0): " + getWeaponList(KEYS[position]).size());
-			fragment = WeaponItemFragment.InstanceOf(getWeaponList(KEYS[position]));
-			break;
-		case 1:
-			fragment = WeaponItemFragment.InstanceOf(getWeaponList(KEYS[position]));
-			break;
-		case 2:
-			fragment = WeaponItemFragment.InstanceOf(getWeaponList(KEYS[position]));
-			break;
-		case 3:
-			fragment = WeaponItemFragment.InstanceOf(getWeaponList(KEYS[position]));
-			break;
-		}
-		return fragment;
-	}
+    private String[] fragment_titles = {"Pistols", "Rifles", "SMG", "Heavy"};
 
-	public ArrayList<Weapon> getWeaponList(String keyPosition) {
-		return (ArrayList<Weapon>) map.get(keyPosition);
-	}
-	@Override
-	public int getItemPosition(Object object) {
-		// TODO Auto-generated method stub
-		return POSITION_NONE;
-	}
+    private HashMap<String, List<Weapon>> map;
 
-	@Override
-	public int getCount() {
-		// TODO Auto-generated method stub
-		return number_tabs;
-	}
+    public WeaponViewPagerAdapter(FragmentManager fm, HashMap<String, List<Weapon>> map) {
+        super(fm);
+        this.map = map;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragment_titles[position];
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        String[] KEYS = {"PISTOLS", "RIFLES", "SMG", "HEAVY"};
+        return WeaponItemFragment.InstanceOf(getWeaponList(KEYS[position]));
+    }
+
+    public ArrayList<Weapon> getWeaponList(String keyPosition) {
+        return (ArrayList<Weapon>) map.get(keyPosition);
+    }
+
+    @Override
+    public int getItemPosition(Object object) {
+        return POSITION_NONE;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
 
 }
